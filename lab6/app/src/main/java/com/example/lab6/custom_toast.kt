ff9845e44c606888553a16f6a958765435b1f73f
benkaric <benkaric2014@gmail.com>
2022-01-10 06:16:08 +0800

    New code

---
+++ lab6/app/src/main/java/com/example/lab6/custom_toast.kt
@@ -0,0 +1,11 @@
+package com.example.lab6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class custom_toast : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.custom_toast)
+    }
+}
